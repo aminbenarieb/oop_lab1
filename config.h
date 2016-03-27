b2bc580e4b42783378b1071a974d8efda219910c
aminbenarieb <amin.benarieb@gmail.com>
2016-03-28 00:32:12 +0300

    Added configuration header.

---
+++ config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define kTextTitle "Carcas Model Vievew"
+
+#define kDrawPenWidth 2
+#define kPadding 20
+
+#define kmin(a, b) ( ( a < b) ? a : b )
+#define kmax(a, b) ( ( a > b) ? a : b )
+#define kRadianToDegree(radian) ( radian*180/M_PI )
+
+
+#endif // CONFIG_H
