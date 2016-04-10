d4027d391f4d95dff74abc4381f140549cf0a97e
aminbenarieb <amin.benarieb@gmail.com>
2016-04-11 01:06:28 +0300

    Steam handler implementation (except handling model data)

--- steamhandler.cpp
+++ steamhandler.cpp
@@ -1,19 +1,16 @@
 #include "steamhandler.h"
+#include "filehandler.h"
 
 ErrorInfo stream_load_model(ModelInfo *model, StreamInfo streamInfo)
 {
-    ErrorInfo error;
+    ErrorInfo error = eOk;
 
     //open file
     error = stream_open_model(streamInfo);
     if (error == eOk){
 
         // processing file
-        error = stream_process_model(streamInfo);
-        if (error == eOk)
-        {
-            // adding data to model
-        }
+        error = stream_handle_model(model, streamInfo);
 
         // closing file
         stream_close_model(streamInfo);
@@ -22,18 +19,55 @@ ErrorInfo stream_load_model(ModelInfo *model, StreamInfo streamInfo)
     return error;
 }
 
-ErrorInfo stream_process_model(StreamInfo streamInfo)
+// MARK: Stream load parts
+
+ErrorInfo stream_handle_model(ModelInfo *model, StreamInfo streamInfo)
 {
-    return eOk;
+    ErrorInfo error = eOk;
+    ModelInfo bufferModelInfo = /*memory allocation*/ModelInfo();
+
+    if (/*bufferModelInfo*/false)
+    {
+        error = file_handle_points(streamInfo.file, /*model->pointarray*/ new PointArrayInfo);
+        if (error == eOk)
+        {
+            error = file_handle_edges(streamInfo.file, /*model->edgesarray*/ new EdgeArrayInfo);
+            if (error == eOk)
+            {
+                //append model to modelStack
+            }
+            else
+            {
+                //release bufferModelInfo
+            }
+        }
+        else
+        {
+            //release bufferModelInfo
+        }
+    }
+    else
+    {
+        error = eOutOfMemory;
+    }
+
+    return error;
 }
 
 ErrorInfo stream_open_model(StreamInfo streamInfo)
 {
+    ErrorInfo error = eOk;
+    streamInfo.file = fopen(streamInfo.filename, "r");
 
+    if (!streamInfo.file)
+    {
+        error = eFileNotFound;
+    }
 
-    return eOk;
+    return error;
 }
 
 void stream_close_model(StreamInfo streamInfo)
 {
+    fclose(streamInfo.file);
 }
--- steamhandler.h
+++ steamhandler.h
@@ -1,16 +1,20 @@
 #ifndef STEAMHANDLER_H
 #define STEAMHANDLER_H
+
+#include <iostream>
 #include "errorinfo.h"
 #include "modelhandler.h"
+
 
 struct StreamInfo
 {
     char *filename;
+    FILE *file;
 };
 
-ErrorInfo stream_load_model(ModelInfo *model, StreamInfo streamInfo);
+ErrorInfo stream_load_model(ModelInfo *, StreamInfo);
 
-ErrorInfo stream_process_model(StreamInfo);
+ErrorInfo stream_handle_model(ModelInfo *, StreamInfo);
 ErrorInfo stream_open_model(StreamInfo);
 void stream_close_model(StreamInfo);
 
