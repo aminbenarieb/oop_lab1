8ce78cbfc76a384b9f2cf64e4e3cf889ef73ce38
aminbenarieb <amin.benarieb@gmail.com>
2016-04-11 00:03:57 +0300

    File loader: Code optimized.

--- fileloader.cpp
+++ fileloader.cpp
@@ -2,33 +2,15 @@
 
 ErrorInfo file_load_count(FILE *file, int &count)
 {
-    ErrorInfo error = eOk;
-    if (fscanf(file, "%d", &count) != 1)
-    {
-        error = eFileCorrupted;
-    }
-
-    return error;
+    return fscanf(file, "%d", &count) == 1 ? eOk : eFileCorrupted;
 }
 
 ErrorInfo file_load_point(FILE *file, PointInfo *point)
 {
-    ErrorInfo error = eOk;
-    if (fscanf(file, "%lf %lf %lf", &point->x, &point->y, &point->z) != 3)
-    {
-        error = eFileCorrupted;
-    }
-
-    return eOk;
+    return fscanf(file, "%lf %lf %lf", &point->x, &point->y, &point->z) == 3 ? eOk : eFileCorrupted;
 }
 
 ErrorInfo file_load_edge(FILE *file, EdgeInfo *edge)
 {
-    ErrorInfo error = eOk;
-    if (fscanf(file, "%d %d", &edge->from, &edge->to) != 2)
-    {
-        error = eFileCorrupted;
-    }
-
-    return eOk;
+    return fscanf(file, "%d %d", &edge->from, &edge->to) == 2 ? eOk : eFileCorrupted;
 }
