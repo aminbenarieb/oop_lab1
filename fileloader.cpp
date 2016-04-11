d19d65686eb4bea9ff9c61964ca362c0b931dae5
aminbenarieb <amin.benarieb@gmail.com>
2016-04-11 04:43:27 +0300

    Restructured fileload module structs in favour modeldata module.
    Restructured fileload module structs in favour modeldata module.
    Params replaced to pointers.

--- fileloader.cpp
+++ fileloader.cpp
@@ -1,16 +1,16 @@
 #include "fileloader.h"
 
-ErrorInfo file_load_count(FILE *file, int &count)
+ErrorInfo file_load_count(FILE *file, int *count)
 {
-    return fscanf(file, "%d", &count) == 1 ? eOk : eFileCorrupted;
+    return fscanf(file, "%d", count) == 1 ? eOk : eFileCorrupted;
 }
 
 ErrorInfo file_load_point(FILE *file, PointInfo *point)
 {
-    return fscanf(file, "%lf %lf %lf", &point->x, &point->y, &point->z) == 3 ? eOk : eFileCorrupted;
+    return fscanf(file, "%lf %lf %lf", &(point->x), &(point->y), &(point->z)) == 3 ? eOk : eFileCorrupted;
 }
 
 ErrorInfo file_load_edge(FILE *file, EdgeInfo *edge)
 {
-    return fscanf(file, "%d %d", &edge->from, &edge->to) == 2 ? eOk : eFileCorrupted;
+    return fscanf(file, "%d %d", &(edge->from), &(edge->to) ) == 2 ? eOk : eFileCorrupted;
 }
--- fileloader.h
+++ fileloader.h
@@ -2,19 +2,10 @@
 #define FILELOADER_H
 
 #include "errorinfo.h"
+#include "modeldata.h"
 #include <iostream>
 
-struct PointInfo
-{
-    double x, y, z;
-};
-struct EdgeInfo
-{
-    int from, to;
-};
-
-
-ErrorInfo file_load_count( FILE *, int &);
+ErrorInfo file_load_count( FILE *, int *);
 ErrorInfo file_load_point( FILE *, PointInfo *);
 ErrorInfo file_load_edge(  FILE *, EdgeInfo *);
 
