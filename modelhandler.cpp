a37e07c036900e41a86c042f3db50dcbc35cd31e
aminbenarieb <amin.benarieb@gmail.com>
2016-04-11 04:46:10 +0300

    Implemented ModeInfo module.

--- modelhandler.cpp
+++ modelhandler.cpp
@@ -1,2 +1,23 @@
 #include "modelhandler.h"
+#include "datahandler.h"
+#include <iostream>
 
+ModelInfo* modelinfo_alloc()
+{
+    ModelInfo* model = new ModelInfo;
+    if (model)
+    {
+        model->edges.count = 0;
+        model->edges.vector = NULL;
+        model->points.count = 0;
+        model->points.vector = NULL;
+    }
+
+    return model;
+}
+
+void modelinfo_dealloc(ModelInfo *model)
+{
+    datahandler_points_dealloc( &(model->points) );
+    datahandler_edges_dealloc(  &(model->edges)  );
+}
--- modelhandler.h
+++ modelhandler.h
@@ -1,6 +1,8 @@
 #ifndef MODELHANDLER_H
 #define MODELHANDLER_H
 
+#include "modeldata.h"
+
 struct MoveInfo
 {
     double dx;
@@ -11,10 +13,15 @@ struct ScaleInfo
 {
     double value;
 };
+
 struct ModelInfo
 {
-   // dump
+    PointVectorInfo points;
+    EdgeVectorInfo  edges;
 };
+
+ModelInfo* modelinfo_alloc();
+void modelinfo_dealloc(ModelInfo *);
 
 
 #endif // MODELHANDLER_H
