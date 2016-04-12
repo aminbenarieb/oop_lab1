ebe267f935cffc4cd2e04b232d399c3f21440269
aminbenarieb <amin.benarieb@gmail.com>
2016-04-12 23:18:24 +0300

    Removed LineInfo structs.

--- converthandler.h
+++ converthandler.h
@@ -4,18 +4,6 @@
 #include "errorinfo.h"
 #include "modelhandler.h"
 
-struct LineInfo
-{
-    PointInfo startPoint;
-    PointInfo endPoint;
-};
-
-struct LineVectorInfo
-{
-    int count;
-    LineInfo *vector;
-};
-
 ErrorInfo convert_alloc_points(PointVectorInfo  *, int);
 void  convert_dealloc_points(PointVectorInfo  *);
 ErrorInfo convert_model_to_points(PointVectorInfo * , ModelInfo, TransformInfo);
